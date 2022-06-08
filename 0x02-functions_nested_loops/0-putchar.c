f99b2200a2d2f85df46f909555b9abafdc1f337c
JC1914 <105009461+JC1914@users.noreply.github.com>
2022-06-08 22:13:14 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,20 @@
-#include <studi.h>
-#include <main.h>
-int main 
-()
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-	printf("_putchar\n");
-	return 0;
+	putchar(95);
+	putchar(112);
+	putchar(117);
+	putchar(116);
+	putchar(99);
+	putchar(104);
+	putchar(97);
+	putchar(114);
+	putchar('\n');
+
+	return (0);
 }
